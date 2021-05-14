21723b7395437bf0243a652f8879e7bb85d1bc34
David J. Fiander <david@fiander.info>
2021-05-14 09:12:25 -0400

    code cleanup

--- keyboards/kbdfans/kbd67/rev2/keymaps/djfiander/keymap.c
+++ keyboards/kbdfans/kbd67/rev2/keymaps/djfiander/keymap.c
@@ -1,84 +1,52 @@
-/*
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
 #include QMK_KEYBOARD_H
 
+// Each layer gets a name for readability, which is then used in the keymap matrix below.
+// The underscores don't mean anything - you can have a layer called STUFF or any other name.
+// Layer names don't all need to be of the same length, obviously, and you can also skip them
+// entirely and just use numbers.
 #define _BL 0
 #define _FL 1
-#define _PL 2
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-/* Keymap (Base Layer) Default Layer
-   * ,----------------------------------------------------------------.
-   * |GEsc| 1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =|Backsp |Del |
-   * |----------------------------------------------------------------|
-   * |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|  \  |Home|
-   * |----------------------------------------------------------------|
-   * |Ctl/Caps|  A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Return|PgUp|
-   * |----------------------------------------------------------------|
-   * |Shift   |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|Shift | Up|PgDn|
-   * |----------------------------------------------------------------|
-   * |FN  |Win |Alt |        Space          |Alt| FN|Ctrl|Lef|Dow|Rig |
-   * `----------------------------------------------------------------'
-   */
-[_BL] = LAYOUT_65_ansi(
-  KC_GESC,  KC_1,    KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_DEL, \
-  KC_TAB,  KC_Q,    KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_HOME, \
-  MT(MOD_LCTL, KC_CAPS), KC_A,    KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,    KC_L,    KC_SCLN, KC_QUOT,  KC_ENT, KC_PGUP,  \
-  KC_LSFT, KC_Z,    KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,   KC_PGDN,           \
-  MO(_FL), KC_LGUI, KC_LALT,                KC_SPC,                          KC_RALT, MO(_FL),   KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT),
+    /* Keymap _BL: (Base Layer) Default Layer
+     * ,----------------------------------------------------------------.
+     * |GEsc| 1|  2|  3|  4|  5|  6|  7|  8|  9|  0|  -|  =|Backsp |Del |
+     * |----------------------------------------------------------------|
+     * |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|  \  |Home|
+     * |----------------------------------------------------------------|
+     * |Ctrl/Caps|A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Return |PgUp|
+     * |----------------------------------------------------------------|
+     * |Shift   |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|Shift | Up|PgDn|
+     * |----------------------------------------------------------------|
+     * | Fn |Win |Alt |        Space          |Alt|Win| Fn |Lef|Dow|Rig |
+     * `----------------------------------------------------------------'
+     */
+    [_BL] = LAYOUT_65_ansi(
+        KC_GESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_DEL,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_HOME,
+        MT(MOD_LCTL, KC_CAPS), KC_A, KC_S, KC_D, KC_F, KC_G,  KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_PGUP,
+        KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,   KC_PGDN,
+        MO(_FL), KC_LGUI, KC_LALT,                            KC_SPC,                    KC_RALT, KC_RGUI, MO(_FL), KC_LEFT, KC_DOWN, KC_RGHT
+    ),
 
-  /* Keymap Fn Layer
-   * ,----------------------------------------------------------------.
-   * |~ `|F1 |F2 |F3 |F4 |F5 |F6 |F7 |F8 |F9 |F10|F11|F12|  Del  |Ins |
-   * |----------------------------------------------------------------|
-   * |     |    |  |   |   |   |   |   |   |   |   |   |   |     |End |
-   * |----------------------------------------------------------------|
-   * |      |   |   |   |   |   |LFT|DN| UP|  RT|   |   |        |    |
-   * |----------------------------------------------------------------|
-   * |        |   |   |   |   |Reset|   |   |   |   |  |Btn1|MsUp|Btn2|
-   * |----------------------------------------------------------------|
-   * |    |MO(PL)|    |                     |   |   |    |MsL|MsD|MsR |
-   * `----------------------------------------------------------------'
-   */
-[_FL] = LAYOUT_65_ansi(
-   KC_GRAVE,  KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9, KC_F10, KC_F11, KC_F12,          _______,KC_INSERT, \
-   _______,_______,  _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,     _______,KC_END,   \
-        _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, _______, _______,          _______, _______, \
-   _______,_______,_______,_______,_______,RESET  ,_______,_______, _______,_______,_______,       KC_MS_BTN1,KC_MS_UP,KC_MS_BTN2, \
-   _______,  MO(_PL), _______,                     _______,                     _______,_______,_______,KC_MS_LEFT,KC_MS_DOWN, KC_MS_RIGHT),
-
-
-  /* Keymap Paging Layer
-   * ,----------------------------------------------------------------.
-   * |   |   |   |   |   |   |   |   |   |   |   |   |   |       |    |
-   * |----------------------------------------------------------------|
-   * |     |    |  |   |   |   |   |   |   |   |   |   |   |     |    |
-   * |----------------------------------------------------------------|
-   * |      |   |   |   |   |   |Home|PgDn|PgUp|End|  |   |      |    |
-   * |----------------------------------------------------------------|
-   * |        |   |   |   |   |     |   |   |   |   |  |    |    |    |
-   * |----------------------------------------------------------------|
-   * |    |    |    |                       |   |   |    |   |   |    |
-   * `----------------------------------------------------------------'
-   */
-[_PL] = LAYOUT_65_ansi(
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
-    _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END, _______, _______,          _______, _______, \
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,            _______, _______, \
-    _______, _______, _______,                            _______,                         _______, _______, _______, _______, _______, _______ \
+    /* Keymap _FL: Function Layer
+     * ,----------------------------------------------------------------.
+     * |   | F1|F2 |F3 |F4 |F5 |F6 |F7 |F8 |F9 |F10|F11|F12|Del    |Ins |
+     * |----------------------------------------------------------------|
+     * |     |   |   |   |   |   |   |   |   |   |PrtScn|  |  |    |End |
+     * |----------------------------------------------------------------|
+     * |      |   |   |   |   |   | <-|Up | Dn| ->|   |   |        |    |
+     * |----------------------------------------------------------------|
+     * |        |   |   |   |   |Reset| |   |   |   |   |   McL|MsU|McR |
+     * |----------------------------------------------------------------|
+     * |    |    |    |                       |   |   |    |MsL|MsD|MsR |
+     * `----------------------------------------------------------------'
+     */
+    [_FL] = LAYOUT_65_ansi(
+        KC_GRAVE, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  KC_INS,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_PSCR, _______, _______, _______, KC_END,
+        _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, _______, _______,          _______, _______,
+        _______,          _______, _______, _______, _______, RESET,   _______, _______, _______, _______, _______, KC_BTN1, KC_MS_U, KC_BTN2,
+        _______, _______, _______,                            _______,                   _______, _______, _______, KC_MS_L, KC_MS_D, KC_MS_R
     ),
 };
