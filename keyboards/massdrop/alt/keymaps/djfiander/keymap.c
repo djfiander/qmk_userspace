67d0d2ab73408c996e98f8db1b309aada7f03c68
David J. Fiander <david@fiander.info>
2021-06-06 12:43:44 -0400

    Replace right Fn key with KC_APP/menu key.

--- keyboards/massdrop/alt/keymaps/djfiander/keymap.c
+++ keyboards/massdrop/alt/keymaps/djfiander/keymap.c
@@ -12,6 +12,7 @@ enum alt_keycodes {
 
 #define _BL 0                   /* base layer */
 #define _FL 1                   /* function layer */
+#define _PL 2                   /* paging layer */
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BL] = LAYOUT_65_ansi_blocker(
@@ -24,9 +25,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_FL] = LAYOUT_65_ansi_blocker(
         KC_GRAVE,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12, KC_DEL, KC_INS, \
         _______, RGB_SPD, RGB_VAI, RGB_SPI, RGB_HUI, RGB_SAI, _______, U_T_AUTO,U_T_AGCR,_______, KC_PSCR, KC_SLCK, KC_PAUS, _______, KC_END, \
-        KC_CAPS, RGB_RMOD,RGB_VAD, RGB_MOD, RGB_HUD, RGB_SAD, _______, _______, _______, _______, _______, _______,          _______, _______, \
+        KC_CAPS, RGB_RMOD,RGB_VAD, RGB_MOD, RGB_HUD, RGB_SAD, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, _______, _______,          _______, _______, \
         _______, RGB_TOG, _______, _______, _______, MD_BOOT, NK_TOGG, DBG_TOG, _______, _______, _______, KC_MS_BTN1,            KC_MS_UP, KC_MS_BTN2, \
-        _______, _______, _______,                            _______,                            _______, _______, KC_MS_LEFT, KC_MS_DOWN, KC_MS_RIGHT  \
+        _______, MO(_PL), _______,                            _______,                            _______, _______, KC_MS_LEFT, KC_MS_DOWN, KC_MS_RIGHT \
+    ),
+    [_PL] = LAYOUT(
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+        _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP,  KC_END, _______, _______,          _______, _______, \
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, \
+        _______, _______, _______,                            _______,                            _______, _______, _______, _______, _______  \
     ),
 /*
     [X] = LAYOUT(
