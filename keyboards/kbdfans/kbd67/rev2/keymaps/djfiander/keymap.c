3fd28d66f44fc7bc3f8916497cdcb3b27e06a7fb
David J. Fiander <david@fiander.info>
2021-10-29 22:07:05 -0400

    All keyboards are the same

--- keyboards/kbdfans/kbd67/rev2/keymaps/djfiander/keymap.c
+++ keyboards/kbdfans/kbd67/rev2/keymaps/djfiander/keymap.c
@@ -15,9 +15,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |----------------------------------------------------------------|
      * |Tab  |  Q|  W|  E|  R|  T|  Y|  U|  I|  O|  P|  [|  ]|  \  |Home|
      * |----------------------------------------------------------------|
-     * |Ctrl/Caps|A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Return |PgUp|
+     * |Ctrl     |A|  S|  D|  F|  G|  H|  J|  K|  L|  ;|  '|Return |PgUp|
      * |----------------------------------------------------------------|
-     * |Shift   |  Z|  X|  C|  V|  B|  N|  M|  ,|  .|  /|Shift | Up|PgDn|
+     * |Shift/( |  Z|  X|  C|  V|  B|  N|  M|  ,|  .| /|Shift/)| Up|PgDn|
      * |----------------------------------------------------------------|
      * | Fn |Win |Alt |        Space          |Alt|Win| Fn |Lef|Dow|Rig |
      * `----------------------------------------------------------------'
@@ -27,7 +27,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_HOME,
         KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G,  KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_PGUP,
         KC_LSPO,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC, KC_UP,   KC_PGDN,
-        MO(_FL), KC_LGUI, KC_LALT,                            KC_SPC,                    KC_RALT, KC_RGUI, KC_APP, KC_LEFT, KC_DOWN, KC_RGHT
+        MO(_FL), KC_LGUI, KC_LALT,                            KC_SPC,                    KC_RALT, KC_RGUI, MO(_FL), KC_LEFT, KC_DOWN, KC_RGHT
     ),
 
     /* Keymap _FL: Function Layer
@@ -38,17 +38,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * |----------------------------------------------------------------|
      * |      |   |   |   |   |   | <-|Up | Dn| ->|   |   |        |    |
      * |----------------------------------------------------------------|
-     * |        |   |   |   |   |Reset| |   |   |   |   |   McL|MsU|McR |
+     * | CapsLk |   |   |   |   |Reset| |   |   |   |   |   McL|MsU|McR |
      * |----------------------------------------------------------------|
-     * |    |    |    |                       |   |   |    |MsL|MsD|MsR |
+     * |    | PL |    |                       |   | PL|    |MsL|MsD|MsR |
      * `----------------------------------------------------------------'
      */
     [_FL] = LAYOUT_65_ansi(
         KC_GRAVE, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______,  KC_INS,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_PSCR, _______, _______, _______, KC_END,
         _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, _______, _______,          _______, _______,
-        _______,          _______, _______, _______, _______, RESET,   _______, _______, _______, _______, _______, KC_BTN1, KC_MS_U, KC_BTN2,
-        _______, MO(_PL), _______,                            _______,                   _______, _______, _______, KC_MS_L, KC_MS_D, KC_MS_R
+        KC_CAPS, _______, _______, _______, _______, RESET,   _______, _______, _______, _______, _______, KC_BTN1, KC_MS_U, KC_BTN2,
+        _______, MO(_PL), _______,                            _______,                   _______, MO(_PL), _______, KC_MS_L, KC_MS_D, KC_MS_R
     ),
     /* Keymap _PL: Paging Layer
      * ,----------------------------------------------------------------.
