750599399d6e4fcb9a48c31cae9aadb94d8fd42d
David J. Fiander <david@fiander.info>
2022-02-20 12:42:19 -0500

    enable bootmagic on kbd67

--- keyboards/kbdfans/kbd67/rev2/keymaps/djfiander/rules.mk
+++ keyboards/kbdfans/kbd67/rev2/keymaps/djfiander/rules.mk
@@ -1,5 +1,5 @@
 # Disable features I'm not interested in
 EXTRAKEY_ENABLE = no	   # Audio control and System control
 CONSOLE_ENABLE = no	   # Console for debugging
-BOOTMAGIC_ENABLE = no
+BOOTMAGIC_ENABLE = yes
 RGBLIGHT_ENABLE = yes
