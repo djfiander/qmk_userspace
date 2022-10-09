90d80b7dcf328ee463e432322f7dc62742a8558e
David J. Fiander <david@fiander.info>
2022-10-09 13:25:15 -0400

    Bring all my keyboards into alignment

--- keyboards/handwired/djfiander/68keys/keymaps/default/keymap.c
+++ keyboards/handwired/djfiander/68keys/keymaps/default/keymap.c
@@ -26,9 +26,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BL] = LAYOUT_65_ansi(
         KC_GESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_DEL,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_HOME,
-        KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G,  KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_PGUP,
+        CTL_T(KC_CAPS), KC_A, KC_S, KC_D, KC_F, KC_G,  KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_PGUP,
         KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,   KC_PGDN,
-        LT(_FL, KC_CAPS), KC_LGUI, KC_LALT,                            KC_SPC,                    KC_RALT, KC_RGUI, MO(_FL), KC_LEFT, KC_DOWN, KC_RGHT
+        MO(_FL), KC_LGUI, KC_LALT,                            KC_SPC,                    KC_RALT, KC_RGUI, MO(_FL), KC_LEFT, KC_DOWN, KC_RGHT
     ),
 
     /* Keymap _FL: Function Layer
@@ -55,7 +55,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * ,----------------------------------------------------------------.
      * |   |   |   |   |   |   |   |   |   |   |   |   |   |       |    |
      * |----------------------------------------------------------------|
-     * |     |   |   |   |   |   |   |   |   |   |      |  |  |    |    |
+     * |     |   |   |   |   |   |   |   |   |   |S(PSCR)| |  |    |    |
      * |----------------------------------------------------------------|
      * |      |   |   |   |   |   | Hm|PgDn|PgUp|End| |   |        |    |
      * |----------------------------------------------------------------|
@@ -66,7 +66,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      */
     [_PL] = LAYOUT_65_ansi(
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  \
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, _______, \
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, S(KC_PSCR), _______, _______,          _______, _______, \
         _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END, _______, _______,          _______,          _______, \
         _______,          _______, _______, _______, _______, _______, _______,   _______, _______, _______, _______,         _______, _______, _______, \
         _______, _______, _______,                   _______,                            _______, _______,         _______,  _______, _______, _______),
