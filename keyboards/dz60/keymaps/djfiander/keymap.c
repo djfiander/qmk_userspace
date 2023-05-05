f73ae0be640d93fb7e251008cf97581955f6d91b
David J. Fiander <david@fiander.info>
2023-05-05 12:42:14 -0400

    Add HYPER home row mod on G and H

--- keyboards/dz60/keymaps/djfiander/keymap.c
+++ keyboards/dz60/keymaps/djfiander/keymap.c
@@ -9,8 +9,10 @@
 #define ALT_S LALT_T(KC_S)
 #define CTL_D LCTL_T(KC_D)
 #define SFT_F LSFT_T(KC_F)
+#define HYPER_G HYPR_T(KC_G)
 
 // Right-hand home row mods
+#define HYPER_H HYPR_T(KC_H)
 #define SFT_J RSFT_T(KC_J)
 #define CTL_K RCTL_T(KC_K)
 #define ALT_L LALT_T(KC_L)
@@ -21,7 +23,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[_BL] = LAYOUT_60_ansi_split_bs_rshift(
 		KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSLS, KC_GRAVE,
 		KC_TAB,           KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSPC,
-		KC_CAPS, GUI_A,   ALT_S,   CTL_D,   SFT_F,   KC_G,    KC_H,    SFT_J,   CTL_K,   ALT_L,   GUI_SCLN, KC_QUOT,             KC_ENT,
+		KC_CAPS, GUI_A,   ALT_S,   CTL_D,   SFT_F,   HYPER_G,    HYPER_H,    SFT_J,   CTL_K,   ALT_L,   GUI_SCLN, KC_QUOT,             KC_ENT,
 		KC_NO,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_NO, KC_NO,
 		KC_NO,   KC_NO, MO(_FL),                   KC_SPC,                                  MO(_FL),   KC_NO,     KC_APP,   KC_NO),
 
