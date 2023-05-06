0d9c84dd8b7d4f27c1755ab64b60b8ef448c2052
David J. Fiander <david@fiander.info>
2023-05-05 21:36:24 -0400

    Fix caps-word when using the shift key to create _

--- users/djfiander/djfiander.c
+++ users/djfiander/djfiander.c
@@ -36,20 +36,22 @@ bool led_update_user(led_t led_state) {
 bool caps_word_press_user(uint16_t keycode) {
     switch (keycode) {
         // Keycodes that continue Caps Word, with shift applied.
-        case KC_A ... KC_Z:
-            add_weak_mods(MOD_BIT(KC_LSFT));  // Apply shift to next key.
-            return true;
+    case KC_A ... KC_Z:
+	add_weak_mods(MOD_BIT(KC_LSFT));  // Apply shift to next key.
+	return true;
 
         // Keycodes that continue Caps Word, without shifting.
-        case KC_1 ... KC_0:
-        case KC_BSPC:
-        case KC_DEL:
-        case KC_UNDS:
-        case KC_MINS:
-            return true;
+    case KC_1 ... KC_0:
+    case KC_BSPC:
+    case KC_DEL:
+    case KC_UNDS:
+    case KC_MINS:
+    case KC_LSFT:
+    case KC_RSFT:
+	return true;
 
-        default:
-            return false;  // Deactivate Caps Word.
+    default:
+	return false;  // Deactivate Caps Word.
     }
 }
 #endif
