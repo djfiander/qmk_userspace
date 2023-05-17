893c01e3b7f651768578d60561b75a1804c6aa20
David J. Fiander <david@fiander.info>
2023-05-17 15:58:03 -0400

    Standardize location of MEH key, and make it layer-tap with the APP key

--- keyboards/dz60/keymaps/djfiander/keymap.c
+++ keyboards/dz60/keymaps/djfiander/keymap.c
@@ -18,6 +18,8 @@
 #define HOME_L LALT_T(KC_L)
 #define HOME_SCLN RGUI_T(KC_SCLN)
 
+#define MEH_APP MEH_T(KC_APP)
+
 #define _BL 0
 #define _FL 1
 #define _RL 2
@@ -29,7 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_TAB,    KC_Q,  KC_W,  KC_E,  KC_R,  KC_T,  KC_Y,  KC_U,  KC_I,  KC_O,  KC_P,  KC_LBRC,  KC_RBRC, KC_BSLS,
 		CTL_CAPS,   KC_A,  KC_S,  KC_D,  KC_F,  KC_G,  KC_H,  KC_J,  KC_K,  KC_L,  KC_SCLN,  KC_QUOT,       KC_ENT,
 		KC_LSFT,     KC_Z,  KC_X,  KC_C,  KC_V,  KC_B,  KC_N,  KC_M,  KC_COMM, KC_DOT,  KC_SLSH,            KC_RSFT,
-		MO(_FL), KC_LGUI, KC_LALT,                  KC_SPC,                   KC_RALT, KC_RGUI, KC_MEH,     MO(_FL)),
+		MO(_FL), KC_LGUI, KC_LALT,                  KC_SPC,                   KC_RALT, KC_RGUI, MEH_APP,    MO(_FL)),
 
 	[_FL] = LAYOUT_60_ansi(
                 _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
--- keyboards/kbdfans/kbd67/rev2/keymaps/djfiander/keymap.c
+++ keyboards/kbdfans/kbd67/rev2/keymaps/djfiander/keymap.c
@@ -4,6 +4,8 @@
 #define CTL_CAPS LCTL_T(KC_CAPS)
 #define CTL_APP  LCTL_T(KC_APP)
 
+#define MEH_APP MEH_T(KC_APP)
+
 // Each layer gets a name for readability, which is then used in the keymap matrix below.
 // The underscores don't mean anything - you can have a layer called STUFF or any other name.
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
@@ -31,7 +33,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,       KC_Q,  KC_W,  KC_E,  KC_R,  KC_T,  KC_Y,  KC_U,  KC_I,  KC_O,  KC_P,  KC_LBRC, KC_RBRC, KC_BSLS,     KC_HOME,
         CTL_CAPS,      KC_A,  KC_S,  KC_D,  KC_F,  KC_G,  KC_H,  KC_J,  KC_K,  KC_L,  KC_SCLN,  KC_QUOT,     KC_ENT,       KC_PGUP,
         KC_LSFT,        KC_Z,  KC_X,  KC_C,  KC_V,  KC_B,  KC_N,  KC_M,  KC_COMM,  KC_DOT,  KC_SLSH,     KC_RSFT, KC_UP,   KC_PGDN,
-        MO(_FL), KC_LGUI, KC_LALT,                            KC_SPC,         KC_RALT, KC_RGUI, KC_MEH,  KC_LEFT, KC_DOWN, KC_RGHT),
+        MO(_FL), KC_LGUI, KC_LALT,                            KC_SPC,         KC_RALT, KC_RGUI, MEH_APP, KC_LEFT, KC_DOWN, KC_RGHT),
 
     /* Keymap _FL: Function Layer
      * ,----------------------------------------------------------------.
