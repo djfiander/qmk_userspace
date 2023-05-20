257cd77869d35776495182c64b317d124bf2edc0
David J. Fiander <david@fiander.info>
2023-05-20 18:57:31 -0400

    Enable caps-word

--- keyboards/handwired/djfiander/68keys/keymaps/default/keymap.c
+++ keyboards/handwired/djfiander/68keys/keymaps/default/keymap.c
@@ -1,9 +1,5 @@
 #include QMK_KEYBOARD_H
 
-// mod-tap keys
-#define CTL_CAPS LCTL_T(KC_CAPS)
-#define CTL_APP  LCTL_T(KC_APP)
-
 // Each layer gets a name for readability, which is then used in the keymap matrix below.
 // The underscores don't mean anything - you can have a layer called STUFF or any other name.
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
@@ -11,6 +7,12 @@
 #define _BL 0
 #define _FL 1
 #define _RL 2
+
+// mod-tap keys
+#define CTL_CAPS LCTL_T(KC_CAPS)
+#define CTL_APP  LCTL_T(KC_APP)
+#define FL_CAPS  LT(_FL, KC_CAPS)
+#define MEH_APP  MEH_T(KC_APP)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Keymap _BL: (Base Layer) Default Layer
@@ -29,9 +31,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BL] = LAYOUT_65_ansi(
         QK_GESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_DEL,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_HOME,
-        CTL_CAPS,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,        KC_ENT,  KC_PGUP,
+        KC_LCTL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,        KC_ENT,  KC_PGUP,
         KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,   KC_PGDN,
-        MO(_FL), KC_LALT,   KC_LGUI,                            KC_SPC,                    KC_RGUI, KC_RALT, MO(_FL), KC_LEFT, KC_DOWN, KC_RGHT
+        FL_CAPS, KC_LGUI,   KC_LALT,                            KC_SPC,                    KC_RALT, KC_RGUI, MEH_APP, KC_LEFT, KC_DOWN, KC_RGHT
     ),
 
     /* Keymap _FL: Function Layer
@@ -52,7 +54,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         LCTL(KC_LALT), _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_PSCR, _______, _______, _______, KC_END,
         _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, _______, _______,          _______, _______,
         KC_MEH,          _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______, _______, KC_MEH, _______, _______,
-        _______, MO(_RL), _______,                   _______,                            _______, MO(_RL),        _______,  _______, _______, _______),
+        _______, MO(_RL), _______,                   _______,                            _______,  _______, MO(_RL),  _______, _______, _______),
 
     /* Keymap _RL: Reset Layer
      * ,----------------------------------------------------------------.
