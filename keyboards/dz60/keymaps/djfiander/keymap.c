cf1c0d6d0ab31fd7eee435fc52814a7db21253cf
David J. Fiander <david@fiander.info>
2023-05-23 08:54:25 -0400

    minor cosmetic source reorganizing

--- keyboards/dz60/keymaps/djfiander/keymap.c
+++ keyboards/dz60/keymaps/djfiander/keymap.c
@@ -1,8 +1,15 @@
 #include QMK_KEYBOARD_H
 
-// mod-tap keys
+//layer names
+#define BASE  0
+#define FUNC  1
+#define RESET 2
+
+// mod/layer-tap keys
 #define CTL_CAPS LCTL_T(KC_CAPS)
 #define CTL_APP  LCTL_T(KC_APP)
+#define MEH_APP MEH_T(KC_APP)
+#define FL_CAPS LT(FUNC, KC_CAPS)
 
 // Left-hand home row mods
 #define HOME_A LGUI_T(KC_A)
@@ -18,12 +25,10 @@
 #define HOME_L LALT_T(KC_L)
 #define HOME_SCLN RGUI_T(KC_SCLN)
 
-#define MEH_APP MEH_T(KC_APP)
-#define FL_CAPS LT(1, KC_CAPS)
-
-#define _BL 0
-#define _FL 1
-#define _RL 2
+// Complicated key shortcuts
+#define CTL_ALT LCTL(KC_LALT)
+#define FUNCL   ML(FUNCTION)
+#define RESETL  ML(RESET)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -36,10 +41,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 	[_FL] = LAYOUT_60_ansi(
                 _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
-		LCTL(KC_LALT),    _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_PSCR, _______, _______, _______,
+		CTL_ALT,    _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_PSCR, _______, _______, _______,
 		_______,          _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,_______, _______, _______,
                 _______,          _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END, _______, _______,          _______,
-		_______, _______, _______,                   _______,                                     _______, _______, _______, MO(_RL)),
+		_______, _______, _______,                   _______,                                     _______, _______, _______, RESETL),
 
         [_RL] = LAYOUT_60_ansi(
                 _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
