4dff7605ebbe73c33e4eb8fa31d1907457e7658d
David J. Fiander <david@fiander.info>
2023-08-19 15:03:22 -0400

    Clean up after merge.

--- keyboards/dz60/keymaps/djfiander/keymap.c
+++ keyboards/dz60/keymaps/djfiander/keymap.c
@@ -27,26 +27,26 @@
 
 // Complicated key shortcuts
 #define CTL_ALT LCTL(KC_LALT)
-#define FUNCL   ML(FUNCTION)
-#define RESETL  ML(RESET)
+#define FUNCL   MO(FUNCTION)
+#define RESETL  MO(RESET)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-	[_BL] = LAYOUT_60_ansi(
+	[BASE] = LAYOUT_60_ansi(
 		QK_GESC,  KC_1,  KC_2,  KC_3,  KC_4,  KC_5,  KC_6,  KC_7,  KC_8,  KC_9,  KC_0,  KC_MINS,  KC_EQL,   KC_BSPC,
 		KC_TAB,    KC_Q,  KC_W,  KC_E,  KC_R,  KC_T,  KC_Y,  KC_U,  KC_I,  KC_O,  KC_P,  KC_LBRC,  KC_RBRC, KC_BSLS,
 		KC_LCTL,    KC_A,  KC_S,  KC_D,  KC_F,  KC_G,  KC_H,  KC_J,  KC_K,  KC_L,  KC_SCLN,  KC_QUOT,       KC_ENT,
 		KC_LSFT,     KC_Z,  KC_X,  KC_C,  KC_V,  KC_B,  KC_N,  KC_M,  KC_COMM, KC_DOT,  KC_SLSH,            KC_RSFT,
 		FL_CAPS, KC_LGUI, KC_LALT,                  KC_SPC,                   KC_RALT, KC_RGUI, MEH_APP,    FL_CAPS),
 
-	[_FL] = LAYOUT_60_ansi(
+	[FUNC] = LAYOUT_60_ansi(
                 _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
 		CTL_ALT,    _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_PSCR, _______, _______, _______,
 		_______,          _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,_______, _______, _______,
                 _______,          _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END, _______, _______,          _______,
 		_______, _______, _______,                   _______,                                     _______, _______, _______, RESETL),
 
-        [_RL] = LAYOUT_60_ansi(
+        [RESET] = LAYOUT_60_ansi(
                 _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
 		_______,          _______, _______, _______, RGB_TOG, _______, _______, _______, _______, _______, _______, _______, _______, _______,
 		_______,          _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______,
