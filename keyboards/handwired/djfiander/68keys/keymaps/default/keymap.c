93aa51a7a046e358981c4c83a5068aeda684d892
David J. Fiander <david@fiander.info>
2023-08-20 14:10:26 -0400

    Implement Space Cadet Parens, MT(CTL, ESC) on all keyboards

--- keyboards/handwired/djfiander/68keys/keymaps/default/keymap.c
+++ keyboards/handwired/djfiander/68keys/keymaps/default/keymap.c
@@ -11,6 +11,7 @@
 // mod-tap keys
 #define CTL_CAPS LCTL_T(KC_CAPS)
 #define CTL_APP  LCTL_T(KC_APP)
+#define CTL_ESC  LCTL_T(KC_ESC)
 #define FL_CAPS  LT(_FL, KC_CAPS)
 #define MEH_APP  MEH_T(KC_APP)
 #define CTL_ENT RCTL_T(KC_ENT)
@@ -30,10 +31,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * `----------------------------------------------------------------'
      */
     [_BL] = LAYOUT_65_ansi(
-        QK_GESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_DEL,
+        KC_GRAVE,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_DEL,
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_HOME,
-        KC_LCTL,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,        CTL_ENT,  KC_PGUP,
-        KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,   KC_PGDN,
+        CTL_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,        CTL_ENT,  KC_PGUP,
+        SC_LSPO,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, SC_RSPC, KC_UP,   KC_PGDN,
         FL_CAPS, KC_LGUI,   KC_LALT,                            KC_SPC,                    KC_RALT, KC_RGUI, MEH_APP, KC_LEFT, KC_DOWN, KC_RGHT
     ),
 
@@ -51,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * `----------------------------------------------------------------'
      */
     [_FL] = LAYOUT_65_ansi(
-        KC_GRAVE, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  KC_INS,
+        _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  KC_INS,
         LCTL(KC_LALT), _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_PSCR, _______, _______, _______, KC_END,
         _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, _______, _______,          _______, _______,
         KC_MEH,          _______, _______, _______, _______, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  _______, _______, KC_MEH, _______, _______,
