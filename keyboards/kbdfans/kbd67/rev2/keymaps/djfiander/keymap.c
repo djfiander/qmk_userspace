1db9960fe5d2fcb7e76e2450b832b1b5a30d2cb6
David J. Fiander <david@fiander.info>
2023-08-21 14:41:05 -0400

    Move mod-tap ESC key from Ctrl to Alt, obviously.

--- keyboards/kbdfans/kbd67/rev2/keymaps/djfiander/keymap.c
+++ keyboards/kbdfans/kbd67/rev2/keymaps/djfiander/keymap.c
@@ -3,7 +3,8 @@
 // mod-tap keys
 #define CTL_CAPS LCTL_T(KC_CAPS)
 #define CTL_APP  LCTL_T(KC_APP)
-#define CTL_ENT RCTL_T(KC_ENT)
+#define ALT_ESC  LALT_T(KC_ESC)
+#define CTL_ENT  RCTL_T(KC_ENT)
 #define CTL_ESC  LCTL_T(KC_ESC)
 
 #define MEH_APP MEH_T(KC_APP)
@@ -34,9 +35,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BL] = LAYOUT_65_ansi(
         KC_GRAVE, KC_1,  KC_2,  KC_3,  KC_4,  KC_5,  KC_6,  KC_7,  KC_8,  KC_9,  KC_0,  KC_MINS, KC_EQL,   KC_BSPC, KC_DEL,
         KC_TAB,   KC_Q,  KC_W,  KC_E,  KC_R,  KC_T,  KC_Y,  KC_U,  KC_I,  KC_O,  KC_P,  KC_LBRC, KC_RBRC, KC_BSLS, KC_HOME,
-        CTL_ESC,   KC_A,  KC_S,  KC_D,  KC_F,  KC_G,  KC_H,  KC_J,  KC_K,  KC_L,  KC_SCLN,  KC_QUOT,       CTL_ENT, KC_PGUP,
+        KC_LCTL,   KC_A,  KC_S,  KC_D,  KC_F,  KC_G,  KC_H,  KC_J,  KC_K,  KC_L,  KC_SCLN,  KC_QUOT,       CTL_ENT, KC_PGUP,
         SC_LSPO,    KC_Z,  KC_X,  KC_C,  KC_V,  KC_B,  KC_N,  KC_M,  KC_COMM,  KC_DOT,  KC_SLSH,   SC_RSPC, KC_UP, KC_PGDN,
-        FL_CAPS, KC_LGUI, KC_LALT,                KC_SPC,             KC_RALT, KC_RGUI, MEH_APP, KC_LEFT, KC_DOWN, KC_RGHT),
+        FL_CAPS, KC_LGUI, ALT_ESC,                KC_SPC,             ALT_ESC, KC_RGUI, MEH_APP, KC_LEFT, KC_DOWN, KC_RGHT),
 
     /* Keymap _FL: Function Layer
      * ,----------------------------------------------------------------.
