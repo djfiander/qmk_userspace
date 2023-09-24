a7334989d00c8f1d3d753f9bbf947dd9986f6be5
David J. Fiander <david@fiander.info>
2023-09-24 16:12:32 -0400

    Initial  lily58 keyboard configuration

---
+++ keyboards/lily58/keymaps/djfiander/config.h
@@ -0,0 +1,13 @@
+
+#pragma once
+
+/* Select hand configuration */
+
+#define MASTER_LEFT
+// #define MASTER_RIGHT
+// #define EE_HANDS
+
+#define QUICK_TAP_TERM 0
+#define TAPPING_TERM 100
+
+#define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
---
+++ keyboards/lily58/keymaps/djfiander/rules.mk
@@ -0,0 +1,16 @@
+CONVERT_TO=promicro_rp2040
+
+CAPS_WORD_ENABLE = yes
+MOUSEKEY_ENABLE = yes        # Mouse keys
+RGBLIGHT_ENABLE = no       # Enable WS2812 RGB underlight.
+SWAP_HANDS_ENABLE = no      # Enable one-hand typing
+OLED_ENABLE= no			     # OLED display
+
+# If you want to change the display of OLED, you need to change here
+SRC +=  ./lib/rgb_state_reader.c \
+        ./lib/layer_state_reader.c \
+        ./lib/logo_reader.c \
+        ./lib/keylogger.c \
+        # ./lib/mode_icon_reader.c \
+        # ./lib/host_led_state_reader.c \
+        # ./lib/timelogger.c \
