7414e4a4b30033039eea934537ff85eab1c1f96a
David J. Fiander <david@fiander.info>
2023-09-26 14:59:49 -0400

    Fix the tapping-term settings so that my MT and LT keys work properly

--- keyboards/lily58/keymaps/djfiander/config.h
+++ keyboards/lily58/keymaps/djfiander/config.h
@@ -7,7 +7,7 @@
 // #define MASTER_RIGHT
 // #define EE_HANDS
 
-#define QUICK_TAP_TERM 0
-#define TAPPING_TERM 100
+/* #define QUICK_TAP_TERM 0 */
+/* #define TAPPING_TERM 100 */
 
 #define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
