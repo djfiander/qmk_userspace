42ab3d80b0cc5bc90a1df2bde0def90ee7e51b78
David J. Fiander <david@fiander.info>
2023-10-01 17:11:39 -0400

    back to CTL/Enter since the left ctrl key on Lily58 is awkward

--- keyboards/lily58/keymaps/djfiander/keymap.c
+++ keyboards/lily58/keymaps/djfiander/keymap.c
@@ -24,7 +24,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_EQL,
   KC_LCTL,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   SC_LSPO,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_LBRC,  KC_RBRC,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  SC_RSPC,
-                        KC_LGUI, ALT_ESC, TL_LOWR, KC_ENT, CTL_SPC, TL_UPPR, KC_BSPC, KC_RALT
+                        KC_LGUI, ALT_ESC, TL_LOWR, CTL_ENT, CTL_SPC, TL_UPPR, KC_BSPC, KC_RALT
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
