a3aa119268c9936046e6667fcf9df101cf636285
David J. Fiander <david@fiander.info>
2024-02-03 14:25:52 -0500

    Enable VIA on Kbd67; make layout comments reflect reality

--- users/djfiander/rules.mk
+++ users/djfiander/rules.mk
@@ -11,5 +11,5 @@ LTO_ENABLE = yes		# Link-time Optimization
 EXTRAKEY_ENABLE = no
 SLEEP_LED_ENABLE= no
 NKRO_ENABLE = no		# NKRO breaks caps lock LED status
-BACKLIGHT_ENABLE = no
+#BACKLIGHT_ENABLE = no
 AUDIO_ENABLE = no
