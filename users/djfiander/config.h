eb23deafb80a3750568324737f05c77b25cce562
David J. Fiander <david@fiander.info>
2024-07-25 10:36:53 -0400

    Move caps-lock off the ctrl key and back to the Fn key

--- users/djfiander/config.h
+++ users/djfiander/config.h
@@ -71,9 +71,9 @@
     [DJF_ANSI_65_BL] = LAYOUT_65_ansi( \
         KC_GRAVE, KC_1,  KC_2,  KC_3,  KC_4,  KC_5,  KC_6,  KC_7,  KC_8,  KC_9,  KC_0,  KC_MINS, KC_EQL,   KC_BSPC, KC_DEL, \
         KC_TAB,   KC_Q,  KC_W,  KC_E,  KC_R,  KC_T,  KC_Y,  KC_U,  KC_I,  KC_O,  KC_P,  KC_LBRC, KC_RBRC, KC_BSLS, KC_HOME, \
-        CTL_CAPS,   KC_A,  KC_S,  KC_D,  KC_F,  KC_G,  KC_H,  KC_J,  KC_K,  KC_L,  KC_SCLN,  KC_QUOT,       CTL_ENT, KC_PGUP, \
+        KC_LCTL,   KC_A,  KC_S,  KC_D,  KC_F,  KC_G,  KC_H,  KC_J,  KC_K,  KC_L,  KC_SCLN,  KC_QUOT,       CTL_ENT, KC_PGUP, \
         SC_LSPO,    KC_Z,  KC_X,  KC_C,  KC_V,  KC_B,  KC_N,  KC_M,  KC_COMM,  KC_DOT,  KC_SLSH,   SC_RSPC, KC_UP, KC_PGDN, \
-        MO(DJF_ANSI_65_FL), KC_LGUI, ALT_ESC,                KC_SPC,             ALT_ESC, KC_RGUI, MEH_APP, KC_LEFT, KC_DOWN, KC_RGHT), \
+        DJF_ANSI_FL_CAPS, KC_LGUI, ALT_ESC,                KC_SPC,             ALT_ESC, KC_RGUI, MEH_APP, KC_LEFT, KC_DOWN, KC_RGHT), \
  \
     /* Keymap DJF_ANSI_65_FL: Function Layer \
      * ,----------------------------------------------------------------. \
